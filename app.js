6676acd81bf486fd59603f76c2e2b0b880f309aa
Chris Urbano <ChrisUrbano1116@gmail.com>
2020-09-03 09:13:24 -0600

    uploading my tv website im working on

---
+++ app.js
@@ -0,0 +1,21 @@
+// Write your JS in here
+var pics = [
+	"imgs/american.jpg",
+	"imgs/Unknown.jpg", 
+	"imgs/images.jpg",
+	"imgs/office.jpg",
+];
+
+//select something on html page
+var btn = document.querySelector("button");
+var img = document.querySelector("img");
+var counter = 1;
+
+btn.addEventListener("click", function(){
+	if(counter === 6)
+	{
+		counter = 0;
+	}
+	img.src = pics[counter];
+	counter++;
+});
